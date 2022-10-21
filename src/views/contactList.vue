10aeb0119ff109f000d99504edeec562b9f491c3
Marcio Miguel <mscanja@gmail.com>
2022-10-21 20:56:57 +0100

    contactList CSS fixed

--- src/views/contactList.vue
+++ src/views/contactList.vue
@@ -1,25 +1,22 @@
 <template>
-    <div class="border border-3  border-white p-5">
+    <div class="border border-3  border-white d-flex flex-column p-5">
         <div >
             <h1 class="text-white p-3">Gestão de Contactos</h1>
         </div>
-        <div class="d-flex flex-row justify-content-between mt-4 align-items-end gap-4"> 
+        <div class="d-flex justify-content-end mt-4 align-items-end gap-4"> 
             <div class="d-grid gap-2 d-md-flex justify-content-md-end">
                 
                 <button type="button" class="btn btn-primary border-white" @click="adicionar">Adicionar</button>
             </div>
         </div>
-        <div class="border border-3 mt-4 border-white p-3">
+        <div class="border border-3 mt-4 border-white d-flex flex-column my-3 p-3">
             
-                <div class="d-flex flex-row border border-2 border-white p-4 gap-2" v-for="(item,i) in getTodos">
-                    <div>
-                        <input class="form-check-input" type="radio" @click="selectTodo(i)" name="flexRadioDefault" id="flexRadioDefault1">    
+                <div class="d-flex flex-row justify-content-between align-items-center border border-2 border-white p-4" v-for="(item,i) in getContacts">
+                    <div class="mx-5">
+                        <p class="text-white">{{item.nome}}</p>
                     </div>
                     <div>
-                        <p class="text-white">{{item.description}}</p>
-                    </div>
-                    <div>
-                        <button class="btn btn-primary border border-white " @click="todoDetail(item)">Detalhe</button>
+                        <button class="btn btn-primary border border-white " @click="contactDetail(item)">Ver</button>
                     </div>
                 </div>
         </div>                
@@ -38,7 +35,7 @@
         },
 
         data(){
-
+            
 
         },
 
@@ -52,7 +49,7 @@
             },
 
             contactDetail(item){
-                this.$router.push({name: "contactDetail", params: {id:item.id, nome:item.nome, telemovel: item.telemovel, email:item.email, morada:item.morada}});
+                this.$router.push({name: "contactDetail", params: {id:item.id, nome:item.nome, telemovel:item.telemovel, email:item.email, morada:item.morada}});
             },
 
             contacts(){
