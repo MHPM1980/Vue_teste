332934aabf624d5b1491398db4f2af07328989be
Marcio Miguel <mscanja@gmail.com>
2022-10-21 19:47:14 +0100

    created addContact.js

---
+++ src/components/models/addContact.js
@@ -0,0 +1,11 @@
+class addContact {
+    constructor() {
+        this.id = null;
+        this.nome = '';
+        this.telemovel = '';
+        this.email = '';
+        this.morada = '';
+    }
+}
+
+export default addContact;
